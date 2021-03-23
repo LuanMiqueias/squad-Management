c218615b389d7ca10ce9af18f390ab9ef794011b
LuanMiqueias <luanmiqueias92@gmail.com>
2021-03-22 23:54:15 -0300

    Adicionado link para home

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./style.module.css";
 
 const Header = () => {
   return (
     <header className={`${styles.header} container`}>
       <div className={`${styles.headerContent} content`}>
-        <div className={styles.logo}>Squad Management Tool</div>
+        <Link to="/" className={styles.logo}>
+          Squad Management Tool
+        </Link>
         <div className={styles.navContainer}>
           <nav>
             <a href="/#">
