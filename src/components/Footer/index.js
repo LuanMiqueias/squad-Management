0f27681167357be8e813b4b1a1d14e1f3fb477be
LuanMiqueias <luanmiqueias92@gmail.com>
2021-03-23 00:35:23 -0300

    Estilizando footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import style from "./style.module.css";
 const Footer = () => {
-  return <footer>{new Date().getFullYear()} - All rigths reserved</footer>;
+  return (
+    <footer className={style.footer}>
+      {new Date().getFullYear()} - All rigths reserved
+    </footer>
+  );
 };
 
 export default Footer;
